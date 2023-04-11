239fe40d3950e8f5410a2f91d9c2232eca9055a1
nitinpratap1111 <you@example.comprpratap.nitin11@gmail.com>
2023-04-11 16:51:10 +0530

    show user functionality

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require ("express")
 const app = express()
+const db = require("./model/connection")
+
+app.use(express.json())
 
 
 app.get("/test",(req,res)=>{
     res.json({"message":"server testing"})
 })
 
+app.post("/addUser",(req,res)=>{
+    //console.log(req.body)
+    const user = ({name:req.body.name,email:req.body.email,phone:req.body.mobile,city:req.body.city})
+    const sql = "INSERT INTO `employee` SET ?"
+    db.query(sql,user,(err,result)=>{
+        if(err) throw err;
+        else res.json(result)
+        
+    }
+    )
+
+})
+
+// showUser
+app.get("/showUser",(req,res)=>{
+    const sql = "SELECT * FROM `employee`"
+    db.query(sql,(err,result)=>{
+        if (err) throw err;
+        else
+        res.json(result)
+    })
+})
+
+app.get("/showUser/:email",(req,res)=>{
+    const sql = `SELECT * FROM employee where email='${req.params.email}'`
+    db.query(sql,(err,result)=>{
+        if(err) throw err;
+        else res.json(result)
+    })
+})
 const PORT= 3002
 app.listen(PORT,()=>console.log("server is running"))
